db2fed734d5a7e3dd95cde6d8b64efcaa9d5cc6a
Alexei Yashkov <alexei.yashkov@yashkov.org>
2026-03-12 07:45:01 -0400

    Specify versions of all providers

--- providers.tf
+++ providers.tf
@@ -3,11 +3,19 @@ terraform {
     hcloud = {
       source  = "hetznercloud/hcloud"
       version = ">= 1.60.1"
-      configuration_aliases = [ hcloud.dns ]
+      configuration_aliases = [hcloud.dns]
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.8.1"
     }
     remote = {
       source  = "tenstad/remote"
       version = ">= 0.2.1"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.2.1"
     }
   }
 }
